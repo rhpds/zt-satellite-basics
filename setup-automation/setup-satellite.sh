df1768f08e3e0e66189a5e18bf52ccd2066b9cd8
Matthew Yee <yee.matthew@gmail.com>
2026-04-14 09:21:36 -0700

    Add commented-out sections for RHEL 10 activation key creation and host registration script

--- setup-automation/setup-satellite.sh
+++ setup-automation/setup-satellite.sh
@@ -8,3 +8,19 @@ hammer host delete --name satellite.lab
 
 # Delete the Activation Key created for the Satellite server.
 hammer activation-key delete --name "Satellite Server" --organization "Acme Org"
+
+# # Create an Activation Key for RHEL 10 content, which will be used by the RHEL 10 client system to register to Satellite and receive the RHEL 10 content.
+# hammer activation-key create --name "RHEL10" --organization "Acme Org" --lifecycle-environment "Library" --content-view "Default Organization View"
+
+# # Create a host registration script.
+# SAT_HOST_REGISTRATION_SCRIPT=$(hammer host-registration generate-command \
+#   --organization "Acme Org" \
+#   --location "Vancouver" \
+#   --activation-keys "RHEL10" \
+#   --insecure true \
+#   --force true \
+#   --setup-insights true \
+#   --setup-remote-execution true)
+
+# ssh root@rhel1.lab bash -c "$SAT_HOST_REGISTRATION_SCRIPT"
+# ssh root@rhel2.lab bash -c "$SAT_HOST_REGISTRATION_SCRIPT"
