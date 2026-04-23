6d7335bd6841d21eec30900c1703ed3ac12bdc2e
Matthew Yee <yee.matthew@gmail.com>
2026-04-23 14:22:06 -0700

    Add systemctl commands to stop and disable dnf-automatic.timer in setup scripts; include git branch command in settings

--- setup-automation/setup-rhel2.sh
+++ setup-automation/setup-rhel2.sh
@@ -3,3 +3,6 @@ dnf remove -y tmux
 systemctl stop dnf-automatic-install.timer
 systemctl disable dnf-automatic-install.timer
 systemctl mask dnf-automatic-install.timer
+
+systemctl stop dnf-automatic.timer
+systemctl disable dnf-automatic.timer
